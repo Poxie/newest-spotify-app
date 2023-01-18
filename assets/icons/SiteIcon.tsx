39849fb65e873dafeb05f0e5677aa8f0ed4c2227
= <poxen12@hotmail.com>
2023-01-18 05:18:25 +0100

    Added header name and icon

---
+++ assets/icons/SiteIcon.tsx
@@ -0,0 +1,8 @@
+export const SiteIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" fill="none">
+        <rect width="44" height="44" rx="7" fill="#FF5959"/>
+        <rect x="12.4526" y="11.6226" width="4.15094" height="19.9245" rx="2.07547" fill="white"/>
+        <rect x="27.396" y="13.283" width="4.15094" height="16.6038" rx="2.07547" fill="white"/>
+        <rect x="19.9243" y="8.30188" width="4.15094" height="27.3962" rx="2.07547" fill="white"/>
+    </svg>
+)
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,8 +1,16 @@
+import styles from '../../styles/Navbar.module.scss';
+import { SiteIcon } from "@/assets/icons/SiteIcon";
+
 export default function Navbar() {
     return(
         <header>
             <nav>
-                navbar
+                <div className={styles['header-name']}>
+                    <SiteIcon />
+                    <span>
+                        {process.env.NEXT_PUBLIC_WEBSITE_NAME}
+                    </span>
+                </div>
             </nav>
         </header>
     )
