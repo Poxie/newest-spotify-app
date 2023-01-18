0ea55d5836a855f196a9dfea155c7b38710fea32
= <poxen12@hotmail.com>
2023-01-18 06:21:48 +0100

    Changed font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import Navbar from '@/components/navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'], weight: ['800', '700', '600', '500'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <>
+    <style jsx global>{`
+      html {
+        font-family: ${inter.style.fontFamily}
+      }
+    `}</style>
     <Navbar />
     <Component {...pageProps} />
     </>
