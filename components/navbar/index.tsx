e82262c7206f7c558f909f8d47c5fe8f2aab5a5f
= <poxen12@hotmail.com>
2023-01-18 06:40:12 +0100

    Allowed navbar to change color depending on if path should be light

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,11 +1,19 @@
+import { useRouter } from 'next/router';
 import styles from '../../styles/Navbar.module.scss';
 import { NavbarName } from './NavbarName';
 import { NavbarProfile } from './NavbarProfile';
 import { NavbarTabs } from './NavbarTabs';
 
+const LIGHT_PATHS = ['/'];
 export default function Navbar() {
+    const router = useRouter();
+
+    const className = [
+        styles['container'],
+        LIGHT_PATHS.find(path => path === router.asPath) ? styles['light'] : ''
+    ].join(' ');
     return(
-        <header className={styles['container']}>
+        <header className={className}>
             <nav className={styles['content']}>
                 <NavbarName />
                 <div className={styles['right']}>
