19d0bc9c5515af12e4805467be0e7241f66edfdb
= <poxen12@hotmail.com>
2023-01-18 06:24:40 +0100

    Added highlighting and underlining

--- components/home/HomeMainText.tsx
+++ components/home/HomeMainText.tsx
@@ -4,7 +4,7 @@ export const HomeMainText = () => {
     return(
         <div className={styles['main-text']}>
             <h1>
-                What does music mean <br/>for you?
+                What does music mean <br/> <span className={'highlighted'}><span className='underlined'>for you</span>?</span>
             </h1>
         </div>
     )
