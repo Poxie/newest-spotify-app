f74606da5ac5fdcae98a042edeb62b8c2056f3eb
= <poxen12@hotmail.com>
2023-01-18 06:31:24 +0100

    Added buttons to home main page

--- components/home/HomeMainText.tsx
+++ components/home/HomeMainText.tsx
@@ -1,4 +1,5 @@
 import styles from '../../styles/Home.module.scss';
+import Button from '../button';
 
 export const HomeMainText = () => {
     return(
@@ -6,6 +7,20 @@ export const HomeMainText = () => {
             <h1>
                 What does music mean <br/> <span className={'highlighted'}><span className='underlined'>for you</span>?</span>
             </h1>
+            <p>
+                Music is different for everyone. Are you interested in exploring others’ taste in music, or would you rather get a better insight into your own? 
+            </p>
+            <div className={styles['buttons']}>
+                <Button href={'/profile'}>
+                    View my stats
+                </Button>
+                <Button 
+                    type={'secondary'}
+                    href={'/explore'}
+                >
+                    Explore mode
+                </Button>
+            </div>
         </div>
     )
 }
