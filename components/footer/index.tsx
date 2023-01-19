979fe504672420c8fb110b03ab643f5355add07f
= <poxen12@hotmail.com>
2023-01-19 06:36:45 +0100

    Made footer responsive

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,9 +3,11 @@ import styles from '../../styles/Footer.module.scss';
 export default function Footer() {
     return(
         <footer className={styles['container']}>
-            <span>
-                All information on this page is fetched from Spotify.
-            </span>
+            <div className={styles['content']}>
+                <span>
+                    All information on this page is fetched from Spotify.
+                </span>
+            </div>
         </footer>
     )
 }
