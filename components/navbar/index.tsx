7d8cd790a52a9a629bf031579cac2aac020bc1a5
= <poxen12@hotmail.com>
2023-01-19 07:06:04 +0100

    Made navbar responsive

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,4 +1,6 @@
+import { HamIcon } from '@/assets/icons/HamIcon';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import styles from '../../styles/Navbar.module.scss';
 import { NavbarName } from './NavbarName';
 import { NavbarProfile } from './NavbarProfile';
@@ -7,10 +9,15 @@ import { NavbarTabs } from './NavbarTabs';
 const LIGHT_PATHS = ['/'];
 export default function Navbar() {
     const router = useRouter();
+    const [open, setOpen] = useState(false);
+
+    // If path change, make sure to close mobile nav
+    useEffect(() => setOpen(false), [router.asPath]);
 
     const className = [
         styles['container'],
-        LIGHT_PATHS.find(path => path === router.asPath) ? styles['light'] : ''
+        LIGHT_PATHS.find(path => path === router.asPath) ? styles['light'] : '',
+        open ? styles['open'] : ''
     ].join(' ');
     return(
         <header className={className}>
@@ -19,6 +26,12 @@ export default function Navbar() {
                 <div className={styles['right']}>
                     <NavbarTabs />
                     <NavbarProfile />
+                </div>
+                <div 
+                    className={styles['ham']}
+                    onClick={() => setOpen(!open)}
+                >
+                    <HamIcon />
                 </div>
             </nav>
         </header>
