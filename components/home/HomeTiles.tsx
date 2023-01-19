b81064dd7cebe5386e5760875a7833c02334b12a
= <poxen12@hotmail.com>
2023-01-19 04:40:11 +0100

    Added wave icon to tiles section

--- components/home/HomeTiles.tsx
+++ components/home/HomeTiles.tsx
@@ -1,23 +1,27 @@
 import styles from '../../styles/Home.module.scss';
 import tiles from '../../assets/static/tiles.json';
 import { HomeTile } from './HomeTile';
+import { WaveIcon } from '@/assets/icons/WaveIcon';
 
 export type TileType = typeof tiles[0];
 export const HomeTiles = () => {
     return(
-        <div className={styles['tiles']}>
-            <h2>
-                All your music needs: right here, <br/>right now.
-            </h2>
+        <section className={styles['tile-section']}>
+            <div className={styles['tiles']}>
+                <h2>
+                    All your music needs: right here, <br/>right now.
+                </h2>
 
-            <ul className={styles['tile-container']}>
-                {tiles.map((tile, key) => (
-                    <HomeTile 
-                        {...tile}
-                        key={key}
-                    />
-                ))}
-            </ul>
-        </div>
+                <ul className={styles['tile-container']}>
+                    {tiles.map((tile, key) => (
+                        <HomeTile 
+                            {...tile}
+                            key={key}
+                        />
+                    ))}
+                </ul>
+            </div>
+            <WaveIcon className={styles['wave-icon']} />
+        </section>
     )
 }
