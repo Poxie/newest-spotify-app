2b7f8a9edebeea44647d994c78346a05094564a1
= <poxen12@hotmail.com>
2023-01-28 12:10:13 +0100

    Added track type

--- types.ts
+++ types.ts
@@ -15,6 +15,28 @@ export type PlayList = {
     uri: string;
 }
 
+export type Track = {
+    album: Album;
+    artists: Artist[];
+    available_markets: string[];
+    disc_number: number;
+    duration_ms: number;
+    episode: boolean;
+    explicit: boolean;
+    external_ids: {isrc: string};
+    external_urls: {spotify: string};
+    href: string;
+    id: string;
+    is_local: boolean;
+    name: string;
+    popularity: number;
+    preview_url: string;
+    track: boolean;
+    track_number: number;
+    type: string
+    uri: string;
+}
+
 export type Album = {
     album_type: string;
     artists: Artist[];
