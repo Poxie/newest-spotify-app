7c9b2dfdb4c4302c27083236a8bf998a62b3d22c
= <poxen12@hotmail.com>
2023-02-07 21:08:11 +0100

    Added typing for environment variables

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,12 @@ declare global {
         interface ProcessEnv {
             NODE_ENV: 'development' | 'production';
             NEXT_PUBLIC_WEBSITE_NAME: string;
+            NEXT_PUBLIC_CLIENT_ID: string;
+            NEXT_PUBLIC_TOKEN_ENDPOINT: string;
+            NEXT_PUBLIC_API_ENDPOINT: string;
+            NEXT_PUBLIC_AUTH_ENDPOINT: string;
+            NEXT_PUBLIC_REDIRECT_URI: string;
+            CLIENT_ID: string;
         }
     }
 }
