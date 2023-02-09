4e19b03af63e8ad0688f9b6de1320c94cb3fac68
= <poxen12@hotmail.com>
2023-02-09 15:38:48 +0100

    If user is not logged in, redirect to login page

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -1,7 +1,25 @@
+import { selectProfileToken, selectProfileTokenLoading } from "@/redux/profile/selectors"
+import { useAppDispatch, useAppSelector } from "@/redux/store"
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Profile() {
+    const router = useRouter();
+    const dispatch = useAppDispatch();
+    const token = useAppSelector(selectProfileToken);
+    const loading = useAppSelector(selectProfileTokenLoading);
+
+    // If user is not logged in, redirect to login page
+    useEffect(() => {
+        if(loading || token) return;
+        router.replace('/login');
+    }, [token, loading]);
+    if(!token) return null;
+
     return(
         <div>
             profile
+            {token}
         </div>
     )
 }
