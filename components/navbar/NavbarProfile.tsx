533f0e50fb3746c898be01d96829aac3da5b65d7
= <poxen12@hotmail.com>
2023-02-09 23:13:52 +0100

    Fixed navbar login/logout button

--- components/navbar/NavbarProfile.tsx
+++ components/navbar/NavbarProfile.tsx
@@ -1,10 +1,34 @@
+import { selectProfileToken, selectProfileTokenLoading } from '@/redux/profile/selectors';
+import { useAppSelector } from '@/redux/store';
 import styles from '../../styles/Navbar.module.scss';
 import Button from "../button"
 
 export const NavbarProfile = () => {
+    const token = useAppSelector(selectProfileToken);
+    const tokenLoading = useAppSelector(selectProfileTokenLoading);
+
+    if(!token || tokenLoading) {
+        return(
+            <Button 
+                className={styles['button']}
+                href={`/login`}
+            >
+                {tokenLoading ? '' : 'Login with Spotify'}
+            </Button>
+        )
+    }
+
+    const signOut = async () => {
+        window.localStorage.clear();
+        window.location.href = window.location.origin;
+    }
+
     return(
-        <Button className={styles['button']}>
-            Login with Spotify
+        <Button 
+            className={styles['button']}
+            onClick={signOut}
+        >
+            Sign out from Spotify
         </Button>
     )
 }
