4b86ad61403c157037e515d4fc98c9c7455c6156
= <poxen12@hotmail.com>
2023-02-10 23:33:37 +0100

    Added missing spotify image domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i.scdn.co']
+    domains: ['i.scdn.co', 'mosaic.scdn.co', 'wrapped-images.spotifycdn.com', 'blend-playlist-covers.spotifycdn.com', 'dailymix-images.scdn.co']
   }
 }
 
