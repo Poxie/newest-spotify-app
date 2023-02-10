d0e98f237987725c3fab92b6644c3864ec4c65bf
= <poxen12@hotmail.com>
2023-02-10 23:34:58 +0100

    Added modal to add a track to playlist

---
+++ modals/playlists/PlaylistItem.tsx
@@ -0,0 +1,50 @@
+import styles from './Playlists.module.scss';
+import { PlayList } from "@/types";
+import Image from 'next/image';
+
+export const PlaylistItem: React.FC<{
+    playlist?: PlayList;
+    active?: boolean;
+    onClick?: (id: string) => void;
+}> = ({ playlist, active, onClick }) => {
+    // If no playlist is given, show placeholder skeleton
+    if(!playlist) {
+        return(
+            <div className={styles['item']}>
+                <div className={styles['item-image']} />
+                <div className={styles['item-text-loading']} />
+            </div>
+        )
+    }
+
+    const { id, name, images } = playlist;
+    const image = images[0]?.url;
+
+    const className = [
+        styles['item'],
+        active ? styles['active'] : ''
+    ].join(' ');
+    return(
+        <button 
+            className={className} 
+            title={name} 
+            aria-label={name}
+            onClick={() => {
+                if(onClick) onClick(id);
+            }}
+        >
+            <div className={styles['item-image']}>
+                {image && (
+                    <Image 
+                        src={image}
+                        alt=""
+                        fill
+                    />
+                )}
+            </div>
+            <span className={styles['item-name']}>
+                {name}
+            </span>
+        </button>
+    )
+}
---
+++ modals/playlists/index.tsx
@@ -0,0 +1,77 @@
+import styles from './Playlists.module.scss';
+import { useAuth } from "@/contexts/auth/AuthProvider";
+import { selectProfileModifyToken } from "@/redux/profile/selectors";
+import { useAppSelector } from "@/redux/store";
+import { PlayList } from "@/types";
+import { useEffect, useState } from "react";
+import Modal from "..";
+import { ModalFooter } from "../ModalFooter";
+import { ModalHeader } from "../ModalHeader";
+import { PlaylistItem } from "./PlaylistItem";
+import { useModal } from '@/contexts/modal/ModalProvider';
+
+const PLACEHOLDER_COUNT = 20;
+export const PlaylistsModal: React.FC<{
+    trackUri: string;
+}> = ({ trackUri }) => {
+    const { close } = useModal();
+    const { get, post } = useAuth();
+    const modifyToken = useAppSelector(selectProfileModifyToken);
+    const [items, setItems] = useState<PlayList[] | null>(null);
+    const [activeItem, setActiveItem] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    // Fetching playlists
+    useEffect(() => {
+        get<{ items: PlayList[] }>(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/me/playlists`)
+            .then(({ items }) => {
+                setItems(items);
+            })
+    }, [modifyToken]);
+
+    // Adding track to playlist
+    const addToPlaylist = async () => {
+        if(!activeItem) return;
+
+        setLoading(true);
+        const response = await post<{ snapshot_id: string }>(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/playlists/${activeItem}/tracks`, { uris: [trackUri] });
+        
+        // Success
+        if(response.snapshot_id) {
+            close();
+            return;
+        }
+    }
+
+    return(
+        <Modal>
+            <ModalHeader>
+                Add track to playlist
+            </ModalHeader>
+            
+            <div className={styles['items']}>
+                {items?.map(item => (
+                    <PlaylistItem 
+                        playlist={item}
+                        active={item.id === activeItem}
+                        onClick={setActiveItem}
+                        key={item.id}
+                    />
+                ))}
+
+                {!items && (
+                    Array.from(Array(PLACEHOLDER_COUNT)).map((_,key) => (
+                        <PlaylistItem key={key} />
+                    ))
+                )}
+            </div>
+
+            <ModalFooter 
+                confirmLabel={'Add to playlist'}
+                onConfirm={activeItem ? addToPlaylist : undefined}
+                confirmDisabled={loading}
+                closeOnCancel
+            />
+        </Modal>
+    )
+}
