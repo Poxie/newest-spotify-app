353d0828190a1dd81e8931b260f8a0e582252fde
= <poxen12@hotmail.com>
2023-02-11 20:14:51 +0100

    Added redirect path for explore page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['i.scdn.co', 'mosaic.scdn.co', 'wrapped-images.spotifycdn.com', 'blend-playlist-covers.spotifycdn.com', 'dailymix-images.scdn.co']
+  },
+  redirects: async function redirects() {
+    return [
+      {
+        source: '/explore',
+        destination: '/explore/songs',
+        permanent: true
+      }
+    ]
   }
 }
 
