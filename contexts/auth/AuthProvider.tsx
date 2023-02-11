0b861a76228ea93e5e0962150a6b5b6b518c35c8
= <poxen12@hotmail.com>
2023-02-11 13:57:58 +0100

    If access token expires, refresh token and fetch data again

--- contexts/auth/AuthProvider.tsx
+++ contexts/auth/AuthProvider.tsx
@@ -2,7 +2,7 @@ import { selectAuthToken } from '@/redux/auth/selectors';
 import { setProfileModifyToken, setProfileTokens } from '@/redux/profile/actions';
 import { selectProfileModifyToken, selectProfileToken } from '@/redux/profile/selectors';
 import { useAppDispatch, useAppSelector } from '@/redux/store';
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement, useCallback, useEffect } from 'react';
 
 const AuthContext = React.createContext({} as {
     get: <T>(query: string, modify?: boolean) => Promise<T>;
@@ -19,6 +19,16 @@ export const AuthProvider: React.FC<{
     const profileToken = useAppSelector(selectProfileToken);
     const modifyToken = useAppSelector(selectProfileModifyToken);
 
+    // Function to refresh token
+    const refreshAccessToken = useCallback(async (refreshToken: string) => {
+        const response = await fetch('/api/refresh', {
+            method: 'POST',
+            body: JSON.stringify({ refreshToken })
+        });
+        const data: { access_token: string } = await response.json();
+        return data;
+    }, []);
+
     // Fetching profile token on mount
     useEffect(() => {
         const profileToken = window.localStorage.token;
@@ -32,23 +42,17 @@ export const AuthProvider: React.FC<{
         }
 
         // Else fetch for refreshed token
-        fetch(`/api/refresh`, {
-            method: 'POST',
-            body: JSON.stringify({ refreshToken })
-        }).then(res => res.json()).then(data => {
-            if(data.error) return;
-            dispatch(setProfileTokens(data.access_token, refreshToken));
-        });
+        refreshAccessToken(refreshToken)
+            .then(({ access_token }) => {
+                dispatch(setProfileTokens(access_token, refreshToken));
+            })
 
         // Fetching for refreshed modify token
         if(modifyRefreshToken) {
-            fetch('/api/refresh', {
-                method: 'POST',
-                body: JSON.stringify({ refreshToken: modifyRefreshToken })
-            }).then(res => res.json()).then(data => {
-                if(data.error) return;
-                dispatch(setProfileModifyToken(data.access_token));
-            })
+            refreshAccessToken(modifyRefreshToken)
+                .then(({ access_token }) => {
+                    dispatch(setProfileModifyToken(access_token));
+                })
         }
     }, []);
 
@@ -64,28 +68,57 @@ export const AuthProvider: React.FC<{
     }, []);
 
     // Function to get information from Spotify
-    const get = async function<T>(query: string, modify?: boolean) {
+    const get = async function<T>(query: string, modify?: boolean, token?: string) {
         const response = await fetch(query, {
             method: 'GET',
             headers: {
-                'Authorization': `Bearer ${(modify ? modifyToken : profileToken) || token}`,
+                'Authorization': `Bearer ${token || (modify ? modifyToken : profileToken) || token}`,
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         });
+
+        // If access token expired
+        if(response.status === 401) {
+            const refreshToken = window.localStorage.refreshToken;
+            const { access_token } = await refreshAccessToken(refreshToken);
+
+            // Updating store with new access token
+            dispatch(setProfileTokens(access_token, refreshToken));
+
+            // Fetching the data again, with new access token
+            const refreshedData: T = await get(query, modify, access_token);
+            return refreshedData;
+        }
+
+        // Else return data
         const data: T = await response.json();
         return data;
     }
 
     // Function to post information to Spotify
-    const post = async function<T>(query: string, body: Object) {
+    const post = async function<T>(query: string, body: Object, token?: string) {
         const response = await fetch(query, {
             method: 'POST',
             headers: {
-                'Authorization': `Bearer ${modifyToken}`,
+                'Authorization': `Bearer ${token || modifyToken}`,
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
             body: JSON.stringify(body)
         });
+
+        // If access token expired
+        if(response.status === 401) {
+            const { access_token } = await  refreshAccessToken(window.localStorage.modifyRefreshToken);
+
+            // Updating store with new modify access token
+            dispatch(setProfileModifyToken(access_token));
+
+            // Fetching the data again, with new access token
+            const refreshedData: T = await post(query, body, access_token);
+            return refreshedData;
+        }
+
+        // Else return data
         const data: T = await response.json();
         return data;
     }
