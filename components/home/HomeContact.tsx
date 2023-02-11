ea76ad9a95ee65f743ba0f9d9a9a128fdcee06d4
= <poxen12@hotmail.com>
2023-02-11 19:06:35 +0100

    Added send message functionality to home contact section

--- components/home/HomeContact.tsx
+++ components/home/HomeContact.tsx
@@ -1,12 +1,40 @@
-import { WaveIcon } from '@/assets/icons/WaveIcon';
-import { useState } from 'react';
+import { useToast } from '@/contexts/toast/ToastProvider';
+import { FormEvent, useRef, useState } from 'react';
 import styles from '../../styles/Home.module.scss';
 import Button from '../button';
 import { Input } from '../input';
 
 export const HomeContact = () => {
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
+    const { setToast } = useToast();
+    const [loading, setLoading] = useState(false);
+    const email = useRef<HTMLInputElement>(null);
+    const message = useRef<HTMLInputElement>(null);
+
+    const sendMessage = async (e: FormEvent) => {
+        if(!email.current || !message.current) return;
+        e.preventDefault();
+
+        setLoading(true);
+        const response = await fetch(`/api/message`, {
+            method: 'POST',
+            body: JSON.stringify({ email: email.current.value, message: message.current.value })
+        });
+        setLoading(false);
+        
+        if(response.status === 200) {
+            setToast({
+                text: 'Thank you for your message.',
+                type: 'success'
+            })
+            email.current.value = '';
+            message.current.value = ''
+        } else {
+            setToast({
+                text: 'Something went wrong. Try again later.',
+                type: 'error'
+            })
+        }
+    }
 
     return(
         <section className={styles['contact-section']}>
@@ -14,24 +42,23 @@ export const HomeContact = () => {
                 <h2>
                     Feel like something is missing?
                 </h2>
-                <form>
+                <form onSubmit={sendMessage}>
                     <Input 
-                        label="Email" 
-                        onChange={setEmail}
-                        value={email}
+                        label="Email or name" 
                         containerClassName={styles['contact-input']}
+                        ref={email}
                     />
                     <Input 
-                        onChange={setMessage}
-                        value={message}
                         label="Message"
                         textArea
+                        ref={message}
                     />
                     <Button 
                         type={'secondary'}
                         buttonType={'submit'}
+                        disabled={loading}
                     >
-                        Send Message
+                        {loading ? 'Sending message...' : 'Send Message'}
                     </Button>
                 </form>
             </div>
