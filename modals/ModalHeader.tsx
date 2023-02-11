2f8acf53ba06a298dda14e678dd00206b8f80d60
= <poxen12@hotmail.com>
2023-02-11 15:46:09 +0100

    Added close button to modal header

--- modals/ModalHeader.tsx
+++ modals/ModalHeader.tsx
@@ -1,11 +1,23 @@
+import { CloseIcon } from '@/assets/icons/CloseIcon';
+import { useModal } from '@/contexts/modal/ModalProvider';
 import styles from './Modals.module.scss';
 
 export const ModalHeader: React.FC<{
     children: any;
 }> = ({ children }) => {
+    const { close } = useModal();
+
     return(
-        <p className={styles['header']}>
-            {children}
-        </p>
+        <div className={styles['header']}>
+            <p className={styles['header-text']}>
+                {children}
+            </p>
+            <button 
+                className={styles['close-button']}
+                onClick={close}
+            >
+                <CloseIcon />
+            </button>
+        </div>
     )
 }
